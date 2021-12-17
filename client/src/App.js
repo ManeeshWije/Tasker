a5076010bc8b5f1e5b5e9a7212541b6448c943b7
ManeeshWije <m.mwije1@gmail.com>
2021-12-17 13:25:14 -0500

    gonna work on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+	return (
+		<div className="App">
+			<h1>Welcome</h1>
+			<h4>Your Current Tasks</h4>
+		</div>
+	);
 }
 
 export default App;
