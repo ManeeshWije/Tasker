bc930a925ea3f3c55e96727c9e5d55fec33a1b88
ManeeshWije <m.mwije1@gmail.com>
2021-12-21 17:28:47 -0500

    think im just gonna host it locally

--- index.js
+++ index.js
@@ -11,59 +11,59 @@ app.use(express.json());
 app.use(cors());
 
 //? MIDDLEWARE
-app.use(express.json());
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+//app.use(express.json());
+//if (process.env.NODE_ENV === "production") {
+//app.use(express.static("client/build"));
+//}
 
 //establishing mongodb connection
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to DB"))
-  .catch(console.error);
+	.connect(process.env.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("Connected to DB"))
+	.catch(console.error);
 
 const Todo = require("./models/Todo");
 
 //display current todos
 app.get("/todos", async (req, res) => {
-  const todos = await Todo.find();
-  res.json(todos);
-  console.log("testing app.get(getting todos)" + todos);
+	const todos = await Todo.find();
+	res.json(todos);
+	console.log("testing app.get(getting todos)" + todos);
 });
 
 //add a todo
 app.post("/todo/new", (req, res) => {
-  const todo = new Todo({
-    text: req.body.text,
-    //complete: req.body.complete,
-  });
-  todo.save();
-  res.json(todo);
-  console.log("testing app.post(new todos)" + todo);
+	const todo = new Todo({
+		text: req.body.text,
+		//complete: req.body.complete,
+	});
+	todo.save();
+	res.json(todo);
+	console.log("testing app.post(new todos)" + todo);
 });
 
 //delete an existing todo by id
 app.delete("/todo/delete/:id", async (req, res) => {
-  const result = await Todo.findByIdAndDelete(req.params.id); //this breaks because of below i think
-  res.json(result);
-  console.log("testing app.delete(delete todo)" + result);
+	const result = await Todo.findByIdAndDelete(req.params.id); //this breaks because of below i think
+	res.json(result);
+	console.log("testing app.delete(delete todo)" + result);
 });
 
 //set completed to an existing todo by id
 app.get("/todo/complete/:id", async (req, res) => {
-  const todo = await Todo.findById(req.params.id);
-  todo.complete = !todo.complete;
-  todo.save();
-  //todo.update();
-  res.json(todo);
-  console.log("testing app.get(complete todo)" + todo);
+	const todo = await Todo.findById(req.params.id);
+	todo.complete = !todo.complete;
+	todo.save();
+	//todo.update();
+	res.json(todo);
+	console.log("testing app.get(complete todo)" + todo);
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+//app.get("*", (req, res) => {
+//res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//});
 
 app.listen(PORT, () => console.log("Server started on port " + PORT));
--- models/Todo.js
+++ models/Todo.js
@@ -3,20 +3,20 @@ const Schema = mongoose.Schema;
 
 //skeleton of a todo
 const TodoSchema = new Schema({
-  //need text
-  text: {
-    type: String,
-  },
-  //true or false for completed
-  complete: {
-    type: Boolean,
-    default: false,
-  },
-  //when it was added
-  timestamp: {
-    type: String,
-    default: Date.now(),
-  },
+	//need text
+	text: {
+		type: String,
+	},
+	//true or false for completed
+	complete: {
+		type: Boolean,
+		default: false,
+	},
+	//when it was added
+	timestamp: {
+		type: String,
+		default: Date.now(),
+	},
 });
 
 const Todo = mongoose.model("Todo", TodoSchema);
