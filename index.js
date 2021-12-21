077b8c50d6b98f6ef9790723b7719d260eecdfdd
ManeeshWije <m.mwije1@gmail.com>
2021-12-21 00:00:06 -0500

    deploy pls

--- index.js
+++ index.js
@@ -58,13 +58,13 @@ app.get("/todo/complete/:id", async (req, res) => {
 
 //? MIDDLEWARE
 app.use(express.json());
-if (process.env.NODE_ENV !== "development") {
-  app.use(express.static("public"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
   // app.use('/static', express.static(path.join(__dirname, 'build')));
 }
 
-//app.get("*", (req, res) => {
-//res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-//});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log("Server started on port " + PORT));
