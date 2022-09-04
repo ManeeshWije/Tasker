4b464f39545c964c8b7e0d8774b57ca3d02b0dce
ManeeshWije <m.mwije1@gmail.com>
2022-09-03 20:55:16 -0400

    add https

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 //base endpoint
-const API = "maneeshs-tasker.fly.dev";
+const API = "https://maneeshs-tasker.fly.dev";
 // const API = "http://localhost:3001";
 
 function App() {
